a96208652bafcc4be9fd7d95a3be1845aaf8c23b
Fabrício Moraes <fabriciomoraes.contato@gmail.com>
2019-06-09 19:15:15 -0300

    SelectionSort algorithm implemented

--- src/BubbleSort/BubbleSort.kt
+++ src/BubbleSort/BubbleSort.kt
@@ -3,29 +3,32 @@ package BubbleSort
 import java.util.*
 
 fun main() {
-    var ret = sort(arrayOf(5,3,2,8,1,4,10,15,11,13,18,20))
+    var ret = sort(arrayOf(5, 3, 2, 8, 1, 4, 10, 15, 11, 13, 18, 20))
     println(Arrays.toString(ret))
 }
 
-//In general the complexity is square O(n²)
-//but if the array was nearly sorted is linear O(n)
-fun sort(arr: Array<Int>) : Array<Int>{
+// In general the complexity is square O(n²)
+// but if the array was nearly sorted is linear O(n)
+fun sort(arr: Array<Int>): Array<Int> {
     var noSwap: Boolean
 
     for (i in arr.size downTo 0 step 1) {
         noSwap = true
-        for(j in 0 until i - 1) {
-            if (arr[j] > arr[j+1]) {
+
+        for (j in 0 until i - 1) {
+            if (arr[j] > arr[j + 1]) {
                 val temp = arr[j]
-                arr[j] = arr[j+1]
-                arr[j+1] = temp
+                arr[j] = arr[j + 1]
+                arr[j + 1] = temp
                 noSwap = false
             }
         }
+
         if (noSwap) {
             break
         }
     }
+
     return arr
 }
 
--- src/SelectionSort/SelectionSort.kt
+++ src/SelectionSort/SelectionSort.kt
@@ -1,9 +1,30 @@
 package SelectionSort
 
+import java.util.*
+
 fun main() {
-    
+    val ret = sort(arrayOf(90, 80, 92, 84, 82, 91, 50, 100, 54, 3, 7, 10, 1))
+    println(Arrays.toString(ret))
 }
 
+// SelectionSort algorithms consists of finding the lowest value in the array and swap this with the value on loop through
+// In general the complexity is square O(n²)
 fun sort(arr: Array<Int>): Array<Int> {
-    return emptyArray()
+    for (i in 0 until arr.size) {
+        var lowest = i
+
+        for (j in i + 1 until arr.size) {
+            if (arr[lowest] > arr[j]) {
+                lowest = j
+            }
+        }
+
+        if (i != lowest) {
+            val temp = arr[i]
+            arr[i] = arr[lowest]
+            arr[lowest] = temp
+        }
+    }
+
+    return arr
 }
