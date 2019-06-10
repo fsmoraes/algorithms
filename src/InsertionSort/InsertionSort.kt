1107abe0022d3c9139ea4a082b95915c3d5b506c
Fabrício Moraes <fabriciomoraes.contato@gmail.com>
2019-06-10 14:14:52 -0300

    InsertionSort algorithms implemented

--- src/InsertionSort/InsertionSort.kt
+++ src/InsertionSort/InsertionSort.kt
@@ -1,9 +1,25 @@
 package InsertionSort
 
-fun main() {
+import java.util.*
 
+fun main() {
+    val ret = sort(arrayOf(2, 1, 9, 80, 4, 10, 24, 75, 66, 99))
+    println(Arrays.toString(ret))
 }
 
 fun sort(arr: Array<Int>): Array<Int> {
+
+    for (i in 0 until arr.size) {
+        val currentVal = arr[i]
+        var j = i - 1
+
+        while (j >= 0 && arr[j] > currentVal) {
+            arr[j + 1] = arr[j]
+            j--
+        }
+
+        arr[j + 1] = currentVal
+    }
+
     return arr
 }
