4a8f1e86a2fd220307be35aa13253e01c8df7670
Fabrício Moraes <fabriciomoraes.contato@gmail.com>
2019-06-11 15:00:42 -0300

    MergeSort algorithms implemented

--- src/MergeSort/MergeSort.kt
+++ src/MergeSort/MergeSort.kt
@@ -1,11 +1,27 @@
 package MergeSort
 
 import java.util.*
+import kotlin.math.floor
+import kotlin.math.roundToInt
 
 fun main() {
-    val ret = merge(arrayOf(1, 2, 3, 12), arrayOf(11, 13, 14))
+    val ret = sort(arrayOf(1, 2, 3, 12, 2, -1, 80, 41))
     println(Arrays.toString(ret))
 }
+
+fun sort(arr: Array<Int>): Array<Int> {
+
+    if (arr.size <= 1) {
+        return arr
+    }
+
+    val middle = floor(arr.size / 2.0).roundToInt()
+    val left = sort(arr.sliceArray(0 until middle))
+    val right = sort(arr.sliceArray(middle until arr.size))
+
+    return merge(left, right)
+}
+
 
 fun merge(arr1: Array<Int>, arr2: Array<Int>): Array<Int> {
     val results = mutableListOf<Int>()
